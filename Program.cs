574bf61631011f619adbf69d510ad2ca28a83fe7
doku228 <164659810+doku228@users.noreply.github.com>
2024-03-23 21:28:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, job,age;
+            Console.WriteLine("как вас зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("кем вы работаете?");
+            job = Console.ReadLine();
+            Console.WriteLine("сколько вам лет?");
+            age = Console.ReadLine();
+            Console.WriteLine("Вас зовут:" + name);
+            Console.WriteLine("ваша работа:" + job);
+            Console.WriteLine("ваш возраст:" +  age);
+            Console.ReadKey();
+
+        }
+    }
+}
